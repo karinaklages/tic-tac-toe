5b3f0428500e2fb997c2a9c829139412494f6e48
Karina Klages <karinaklages@web.de>
2025-11-25 14:32:31 +0100

    refactor: render function added

---
+++ script.js
@@ -0,0 +1,37 @@
+let fields = [
+    null,
+    'circle',
+    'circle',
+    'circle',
+    null,
+    null,
+    'cross',
+    'cross',
+    null
+];
+
+function init() {
+    render();
+}
+
+function render() {
+    let html = "<table>";
+
+    for (let row = 0; row < 3; row++) {
+        html += "<tr>";
+
+        for (let col = 0; col < 3; col++) {
+            let index = row * 3 + col;
+            let value = fields[index];
+            let symbol = "";
+            if (value === "circle") symbol = "O";
+            if (value === "cross") symbol = "+";
+            html += `<td>${symbol}</td>`;
+        }
+        html += "</tr>";
+    }
+    html += "</table>";
+    document.getElementById("content").innerHTML = html;
+}
+
+render();
